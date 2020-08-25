c7c55aa98237b27c680fe2ee5bc22db4e059b2d6
ginami0129g <ginami0129n@naver.com>
2020-08-25 19:01:04 +0900

    Add 002_ing

---
+++ vue.js_lecture/002_vue_intro/02_ing/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<div id="app">
+		
+	</div>
+</template>
+
+<script>
+import UserList from "./components/UserList";
+import AddUser from "./components/AddUser";
+export default {
+	components: {
+		UserList,
+		AddUser
+	},
+
+	
+};
+</script>
+
+<style>
+#app {
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
---
+++ vue.js_lecture/002_vue_intro/02_ing/src/components/AddUser.vue
@@ -0,0 +1,28 @@
+<template>
+    ??
+</template>
+
+<script>
+    export default {
+        ??
+
+    }
+</script>
+
+<style scoped>
+    .add-user {
+        height:40px;
+        align-items:center;
+        display:flex;
+    }
+    .add-user > * {
+        height:100%;
+    }
+
+    .add-user > input {
+        box-sizing: border-box;
+        flex:1;
+    }
+
+
+</style>
