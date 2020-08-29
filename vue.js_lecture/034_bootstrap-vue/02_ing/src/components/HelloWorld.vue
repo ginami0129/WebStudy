204e8db17c637dd44ef32c5480dab768574550fa
ginami0129g <ginami0129n@naver.com>
2020-08-29 23:43:32 +0900

    Learn 034_bootstrap-vue

--- vue.js_lecture/034_bootstrap-vue/02_ing/src/components/HelloWorld.vue
+++ vue.js_lecture/034_bootstrap-vue/02_ing/src/components/HelloWorld.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
-    //??
+    <div>
+  <b-button>Button</b-button>
+  <b-button variant="danger">Button</b-button>
+  <b-button variant="success">Button</b-button>
+  <b-button variant="outline-primary">Button</b-button>
+</div>
+
+<div class="text-center">
+  <b-button variant="primary">
+    Profile
+    <b-badge variant="light">9 <span class="sr-only">unread messages</span></b-badge>
+  </b-button>
+</div>
   </div>
 </template>
 
