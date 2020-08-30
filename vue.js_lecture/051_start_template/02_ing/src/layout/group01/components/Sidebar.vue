3d12a66dba00c8302c81f09ca596eb4a5a51433a
ginami0129g <ginami0129n@naver.com>
2020-08-30 18:44:22 +0900

    Learn 051_start_template

--- vue.js_lecture/051_start_template/02_ing/src/layout/group01/components/Sidebar.vue
+++ vue.js_lecture/051_start_template/02_ing/src/layout/group01/components/Sidebar.vue
@@ -1,7 +1,7 @@
 <template>
 		<b-list-group>
 			<b-list-group-item to="/group01/board" key="board">Board</b-list-group-item>
-			<!--?? Sample 추가-->
+			<b-list-group-item to="/group01/sample" key="sample">Sample</b-list-group-item>
 		</b-list-group>
 </template>
 
--- vue.js_lecture/051_start_template/02_ing/src/router/modules/group01Router.js
+++ vue.js_lecture/051_start_template/02_ing/src/router/modules/group01Router.js
@@ -1,5 +1,6 @@
 import Group01Layout from "@/layout/group01";
 import Board from "@/views/group01/Board";
+import Sample from "@/views/group01/Sample";
 
 const group01Router = { 
 	path:"/group01",
@@ -14,8 +15,11 @@ const group01Router = {
 			path: "board",
 			name:"group01Board",
 			component: Board,
-		}
-		//??
+		},{
+      path:"sample",
+      name:"group01Sample",
+      component:Sample
+    }
 	],
 }
 
